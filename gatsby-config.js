95d341a0c8c410c7e7c32a744846dd3f541259bb
yusufcodes <yusufcodes@outlook.com>
2021-02-07 10:36:30 +0000

    Updated contentful plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         spaceId: `uwoaomvuao0g`,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: `cdn.contentful.com`,
       },
     },
     `@contentful/gatsby-transformer-contentful-richtext`,
